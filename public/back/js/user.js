e172db448f410cf5955ed59d3e7c34d48fe0889a
xxfuturexx <2084883515@qq.com>
2019-10-10 21:53:12 +0800

    用户管理-分页渲染完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,17 +1,35 @@
 // 2019/10/10
 $(function(){
-   $.ajax({
-      type:"get",
-       url:"/user/queryUser",
-       data:{
-          page: 1,
-           pageSize: 10
-       },
-       dataType:"json",
-       success:function(info){
-          console.log(info);
-          var  htmlStr = template( 'tpl' , info);
-          $('tbody').html(htmlStr);
-       }
-   })
+    var currentPage = 1;
+    var pageSize = 5;
+    render();
+    function render(){
+        $.ajax({
+            type:"get",
+            url:"/user/queryUser",
+            data:{
+                page: currentPage,
+                pageSize: pageSize
+            },
+            dataType:"json",
+            success:function(info){
+                console.log(info);
+                var  htmlStr = template( 'tpl' , info);
+                $('tbody').html(htmlStr);
+
+                //分页初始化
+                $('#paginator').bootstrapPaginator({
+                    bootstrapMajorVersion: 3,
+                    totalPages: Math.ceil( info.total/info.size),
+                    currentPage: info.page,
+                    onPageClicked: function (a,b,c,page) {
+                        currentPage = page;
+                        render();
+                    }
+
+                })
+            }
+        })
+    }
+
 });
